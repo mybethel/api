d1836c537319af4cd9b90dac4186fd215fca4773
Albert Martin <albert@imor.tl>
2016-12-03 17:47:08 -0600

    feat(media): add mime type to API response

--- api/media/media.model.js
+++ api/media/media.model.js
@@ -1,15 +1,19 @@
+const mime = require('mime');
 const Mongoose = require('mongoose');
 
 module.exports = {
   schema: {
     name: String,
     podcast: { type: Mongoose.Schema.Types.ObjectId, ref: 'Podcast' },
+    size: Number,
     url: String
   },
   options: {
     collection: 'podcastmedia',
     toJSON: {
       transform(doc, ret, options) {
+        ret.size = ret.size || 0;
+        ret.type = mime.lookup(ret.url);
         ret.url = doc.downloadUrl;
         return ret;
       }
