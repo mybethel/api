ffa733e787b32e58e413fd9d788fc486b66ba7c0
Albert Martin <albert@imor.tl>
2017-01-21 11:59:43 -0600

    chore(general): update nautilus dependency

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (process.env.NEW_RELIC_LICENSE_KEY) {
   require('newrelic');
 }
 
-const Nautilus = require('nautilus');
+const Nautilus = require('@nautilus/web');
 var server = new Nautilus({
   session: false,
   slash: false
