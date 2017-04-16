629d8339e0d5d0e095d247737d7bec6279fef789
Albert Martin <albert@imor.tl>
2017-04-16 12:31:26 -0500

    chore(performance): trust the reverse proxy for visitor IP

--- hooks/performance.js
+++ hooks/performance.js
@@ -1,5 +1,9 @@
 const Keen = require('keen-js');
 module.exports = app => {
+  // Ensure that visitor IP addresses are not the reverse proxy to ensure we get
+  // the most accurate data before logging to Keen.
+  app.enable('trust proxy');
+
   try {
     return new Keen(app.config.performance);
   } catch(err) {
