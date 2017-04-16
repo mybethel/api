2b22281864485b1e9bfc7f89a5f31a4215b77cb4
Albert Martin <albert@imor.tl>
2017-04-16 12:22:29 -0500

    feat(performance): expand IP and user agent fields

--- api/performance/performance.controller.js
+++ api/performance/performance.controller.js
@@ -10,10 +10,30 @@ module.exports = (router, app) => ({
     let collection = req.body.collection;
     delete req.body.collection;
 
+    let addons = [];
+
+    if (req.body.ip_address) {
+      addons.push({
+        name: 'keen:ip_to_geo',
+        input: { ip: 'ip_address' },
+        output: 'ip_geo_info',
+      });
+    }
+
+    if (req.body.user_agent) {
+      addons.push({
+        name: 'keen:ua_parser',
+        input: { ua_string: 'user_agent' },
+        output: 'parsed_user_agent',
+      });
+    }
+
+    req.body.keen = { addons };
+
     // Analytics are sent behind the scenes and a `200` response is always sent.
     // This is done to keep the API responsive and prevent upstream issues from
     // affecting the usability of end-user applications.
-    app.analytics.addEvent(collection, req.body, err => {
+    app.performance.addEvent(collection, req.body, err => {
       if (err) app.log.error(err);
     });
 
