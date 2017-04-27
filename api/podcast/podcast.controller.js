d82e8202a4d6b54868450318ef075c8f9e2bec8c
Albert Martin <albert@imor.tl>
2017-04-26 22:50:43 -0500

    chore(performance): switch to using signed URL for performance queries

--- api/podcast/podcast.controller.js
+++ api/podcast/podcast.controller.js
@@ -7,7 +7,7 @@ module.exports = (router, app) => ({
       .then(formatted => res.json(formatted));
   },
 
-  '/:id/stats': function(req, res) {
+  '/:id/performance': function(req, res) {
     let subscribers = app.performance.query('count', {
       event_collection: 'podcast',
       filters: [{
@@ -19,28 +19,12 @@ module.exports = (router, app) => ({
       timeframe: 'this_12_weeks',
     });
 
-    // @TODO: Anything with a graph is more performant if queries with an XHR
-    // request directly on the frontend. In the future, rather than running the
-    // query in the API we should simply return a pre-signed JSONP request.
     Promise.all([
-      function queryAnalytics() {
-        return new Promise((resolve, reject) => {
-          app.performance.client.run(subscribers, function(err, response) {
-            if (err) return reject(err);
-            resolve(response.result);
-          });
-        })
-      }(),
       app.model('media').find({ podcast: req.params.id }).count(),
     ]).then(results => {
-      // Subscriber count is the total number of subscribers as counted last
-      // week. Numbers for the current week are too volatile as we only have a
-      // partial representation of the entire week.
-      let subscribers = results[0].reverse()[1].value || '0';
       res.ok({
         podcast: req.params.id,
-        episodes: results[1],
-        graph: results[0],
+        episodes: results[0],
         subscribers,
       });
     });
--- hooks/performance.js
+++ hooks/performance.js
@@ -1,4 +1,6 @@
 const Keen = require('keen-js');
+const qs = require('qs');
+
 module.exports = app => {
   // Ensure that visitor IP addresses are not the reverse proxy to ensure we get
   // the most accurate data before logging to Keen.
@@ -8,7 +10,10 @@ module.exports = app => {
   return {
     client,
     query(type, config) {
-      return new Keen.Query(type, config);
+      let endpoint = `projects/${app.config.performance.projectId}/queries/${type}`;
+      config['api_key'] = app.config.performance.readKey;
+      config = qs.stringify(config);
+      return `https://api.keen.io/3.0/${endpoint}?${config}`;
     }
   };
 };
