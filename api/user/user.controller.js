eae2edf82bcfb14ddcfa2b493cdfd39aa9788667
Albert Martin <albert@imor.tl>
2017-05-12 19:56:30 -0500

    feat(user): allow user to masquerade as a different account

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,10 +1,45 @@
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 module.exports = (router, app) => ({
 
-  // If there is a valid token, re-issue token with an updated expiration time.
-  // If no token, check for valid username and password and then issue a new token.
+  /**
+   * Get the ministries for which a particular user is authorized to manage.
+   * For users with `staff` level priveleges, a full list of all ministries will
+   * be returned in order to allow masquerading as any ministry.
+   * @param {ObjectID} req.params.id - The user ID to return ministries for.
+   */
+  '/:id/ministries': function(req, res) {
+    app.model('user').findOne({ _id: req.params.id }).then(user => {
+      if (!(user = user.toJSON())) {
+        return res.unauthorized();
+      }
+
+      let query = {};
+      if (user.permission !== 'staff') {
+        let authorized = user.ministriesAuthorized || [];
+        authorized.push(user.ministry);
+        query = { _id: { $in: authorized }};
+      }
+
+      app.model('ministry').find(query)
+        .select('_id name').sort('name').lean()
+        .then(ministries => {
+          res.ok(ministries);
+        });
+    });
+  },
+
+  /**
+   * Deliver an authentication token for the user to query the API for protected
+   * resources. Requires an existing, valid token or a username and password
+   * pair in order to deliver a new token. Tokens are valid for one week.
+   * Optionally, a ministry ID can be included to allow the user to masquerade
+   * as a ministry other than their primary association.
+   * @param {String} req.authorization - See the `token` middleware.
+   * @param {String} req.body.email - The e-mail address of the user to login.
+   * @param {String} req.body.password - The user password to validate.
+   * @param {String} req.body.ministry - (optional) The ministry ID to use.
+   */
   auth(req, res) {
     let hasToken = !!req.authorization;
     if (!hasToken && (!req.body.email || !req.body.password)) {
@@ -18,12 +53,19 @@ module.exports = (router, app) => ({
         return res.unauthorized();
       }
 
-      const token = jwt.sign({
-        id: user._id,
-        permission: user.permission,
-      }, app.config.security.jwtSecret, {
-        expiresIn: '7d',
-      });
+      // Allow the user to masquerade under a ministry other than their primary.
+      // Requires the user to have staff permissions or to have the ministry ID
+      // listed as authorized on their account.
+      if (req.body.ministry) {
+        let userObject = user.toJSON();
+        if (userObject.permission !== 'staff' && userObject.ministriesAuthorized.indexOf(req.body.ministry) < 0) {
+          return res.unauthorized();
+        }
+
+        user.ministry = req.body.ministry;
+      }
+
+      const token = app.token.issue(user);
 
       if (hasToken) return res.ok({ data: user, token });
 
