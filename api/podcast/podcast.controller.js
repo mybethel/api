0ff2ac59bd4180c04b69cf0472fcf661d8b0c32e
Albert Martin <albert@imor.tl>
2018-02-12 15:18:57 -0600

    fix(podcast): prevent deleted episodes from showing in feed

--- api/podcast/podcast.controller.js
+++ api/podcast/podcast.controller.js
@@ -1,7 +1,7 @@
 module.exports = (router, app) => ({
 
   '/:id/media': function(req, res) {
-    let query = app.blueprint.find('media', { podcast: req.params.id }, req);
+    let query = app.blueprint.find('media', { deleted: { $ne: true }, podcast: req.params.id }, req);
 
     query.then(results => app.blueprint.format(results, query, req))
       .then(formatted => res.ok(formatted))
